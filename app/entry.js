dc540d5866c3393ded71a824961cb7d92f1bb3d6
Greg <projects@gregoire-audoux.fr>
2015-11-08 12:53:11 +0100

    Le routage basic est en place

--- app/entry.js
+++ app/entry.js
@@ -1,6 +1,8 @@
+//loading libs
+var angular = require('angular');
+
 // require assets
-require('file?name=index.html!index.html')
+require('file?name=index.html!index.html');
 
 // require modules
 require('stats.core');
-require('sLogger');
