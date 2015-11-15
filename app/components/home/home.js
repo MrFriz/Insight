63446b59db268aaaeef4bd5999fca472aa9949f9
Grégoire Audoux <projects@gregoire-audoux.fr>
2015-11-16 00:06:33 +0100

    On gère un player youtube sur la homepage

--- app/components/home/home.js
+++ app/components/home/home.js
@@ -1,4 +1,6 @@
 module.exports = function HomeController($scope) {
     "ngInject"
-    $scope.test = 'Yo les amis !';
+
+    $scope.ytUrl = 'https://www.youtube.com/watch?v=A3PDXmYoF5U';
+    $scope.ytId = 'A3PDXmYoF5U';
 }
