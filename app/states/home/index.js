6295adf1abf6602bfbf04479c9764615cb3e2126
Grégoire Audoux <projects@gregoire-audoux.fr>
2015-12-01 00:23:01 +0100

    On gère un peut mieux la liste des match sur la home

--- app/states/home/index.js
+++ app/states/home/index.js
@@ -1,45 +1,66 @@
-function HomeController($state, $rootScope, $scope, GameLogs) {
-
-    GameLogs.listGames().then(function (games) {
-        $scope.games = games.docs;
-    });
-
-
-    // TODO: we need real ID here
-    if (!$rootScope.currentID) {
-        $rootScope.currentID = 1;
-    }
-
-
-    $scope.$state = $state;
-    this.i = {
-        $state,
-        GameLogs
-    };
-}
-
-HomeController.prototype.openGame = function (id) {
-    this.i.$state.go('record', {id: id});
-}
-
-HomeController.prototype.createGame = function () {
-    this.i.GameLogs.create()
-        .then((gameDoc) => {
-            this.openGame(gameDoc.id);
-        });
-};
-
-HomeController.prototype.deleteGame = function (id) {
-    this.i.GameLogs.delete(id).finally(()=> {
-        GameLogs.listGames().then(function (games) {
-            $scope.games = games.docs;
-        })
-    })
-};
-
-
 angular.module(require('insight.module')).
 config(function ($stateProvider) {
+
+    function HomeController($state, $rootScope, $scope, GameLogs, $mdDialog, $log) {
+
+        this._log = $log;
+        this._mdDialog = $mdDialog;
+        this._scope = $scope;
+
+        GameLogs.listGames().then(function (games) {
+            $scope.games = games.docs;
+        });
+
+
+        // TODO: we need real ID here
+        if (!$rootScope.currentID) {
+            $rootScope.currentID = 1;
+        }
+
+
+        $scope.$state = $state;
+        this.i = {
+            $state,
+            GameLogs
+        };
+    }
+
+    HomeController.prototype.openGame = function (id) {
+        this.i.$state.go('record', {id: id});
+    };
+
+    HomeController.prototype.createGame = function () {
+        this.i.GameLogs.create()
+            .then((gameDoc) => {
+                this.openGame(gameDoc.id);
+            });
+    };
+
+    HomeController.prototype.deleteGame = function ($event, id) {
+
+
+        var confirm = this._mdDialog.confirm()
+            .clickOutsideToClose(true)
+            .title('Suppression')
+            .textContent('Êtes-vous sûr de vouloir supprimer le match ?')
+            .targetEvent($event)
+            .ok('Oui')
+            .cancel('Non');
+
+        this._mdDialog.show(confirm)
+            .then(() => {
+                return this.i.GameLogs.delete(id)
+            })
+            .then(()=> {
+                return this.i.GameLogs.listGames()
+            })
+            .then((games) => {
+                this._scope.games = games.docs;
+            });
+
+
+    };
+
 
     $stateProvider.state(
         'home', {
