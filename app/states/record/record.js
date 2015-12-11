6a079bdc55fb5c420b5d6a1fcd00bee1f37f891d
Grégoire Audoux <projects@gregoire-audoux.fr>
2015-12-11 19:18:46 +0100

    Ok on simplifie toutes les conneries que j'ai fait sur le team  - si on veux recupéré la liste des team il faudra faite une requette sur les objet de type 'game' et trouver les team1/team2 unique

--- app/states/record/record.js
+++ app/states/record/record.js
@@ -3,52 +3,14 @@ function RecordController($scope, game, teams, DataStore) {
 
     this.DataStore = DataStore;
     this.game = game;
-    this.teams = teams;
-
-    if (this.game.team1) {
-        this.DataStore.teams(this.game.team1).then(
-            (team) => {
-                $scope.team1 = team;
-            }
-        )
-    }
-
-    if (this.game.team2) {
-        this.DataStore.teams(this.game.team2).then(
-            (team) => {
-                $scope.team2 = team;
-            }
-        )
-    }
 
 
-    $scope.createNewTeam = (name) => {
-        console.log('createNewTeam()', name);
-    }
 }
 
-RecordController.prototype.updateTeam = function (team, isTeam2) {
-    var teamProps = isTeam2 ? 'team2' : 'team1';
-
-    var teamId = team ? team._id : undefined;
-
-    // fail early
-    if (!team && !this.game[teamProps]) return;
-    if (team && team._id === this.game[teamProps]) return;
-
-
-    this.DataStore.games(this.game._id)
-        .then((game) => {
-            game[teamProps] = team ? team._id : undefined;
-            return this.DataStore._put(game)
-        })
-        .then((res) => {
-            return this.DataStore.games(res.id);
-        })
-        .then((game)=> {
-            this.game = game;
-        })
-
+RecordController.prototype.updateGame = function () {
+    return this.DataStore._put(this.game).then((updatedGame) => {
+        this.game = updatedGame;
+    });
 };
 
 
